548c48ce518736f6a883895af66343e6f55648b8
Colin Dunklau <colin.dunklau@gmail.com>
2016-03-01 12:12:29 -0600

    Another router test, DRY fake view function

--- tests/router_spec.js
+++ tests/router_spec.js
@@ -1,11 +1,14 @@
 var Router = require('../lib/router');
 
+
+var fakeView = function fakeView(request) {};
+
 describe('A Router object', function() {
   var router;
   beforeEach(function() {
     router = new Router();
-    router.addRoute('index', '/', function() {});
-    router.addRoute('posts:index', '/posts', function() {});
+    router.addRoute('index', '/', fakeView);
+    router.addRoute('posts:index', '/posts', fakeView);
   });
 
   it('resolves routes based on the URL path', function() {
@@ -15,40 +18,49 @@ describe('A Router object', function() {
 
   it('allows adding routes without intermediate routes in between',
           function() {
-    router.addRoute('people:index', '/api/people', function() {});
+    router.addRoute('people:index', '/api/people', fakeView);
     expect(router.getMatch('/api/people').route.name).toBe('people:index');
   });
 
   it('throws on duplicate route names', function() {
-    expect(router.addRoute.bind(router, 'index', '/newindex', function() {})).
+    expect(router.addRoute.bind(router, 'index', '/newindex', fakeView)).
           toThrowError(/Route already exists with name/);
   });
 
   it('throws on duplicate patterns', function() {
-    expect(router.addRoute.bind(router, 'newindexname', '/', function() {})).
+    expect(router.addRoute.bind(router, 'newindexname', '/', fakeView)).
           toThrowError(/Route already exists at/);
   });
 
-  it('resolves URL paths matching replacement markers', function() {
-    router.addRoute('posts:detail', '/posts/{postId}', function() {});
+  it('resolves a URL path matching a replacement marker', function() {
+    router.addRoute('posts:detail', '/posts/{postId}', fakeView);
     var match = router.getMatch('/posts/123');
     expect(match.route.name).toBe('posts:detail');
     expect(match.fields).toEqual({postId: '123',});
   });
 
-  it('resolves URL paths matching multiple replacement markers', function() {
+  it('resolves a URL path matching multiple replacement markers', function() {
     router.addRoute(
-      'posts:detail', '/posts/{postId}/history/{revision}', function() {}
+      'posts:detail', '/posts/{postId}/history/{revision}', fakeView
     );
     var match = router.getMatch('/posts/123/history/456');
     expect(match.route.name).toBe('posts:detail');
     expect(match.fields).toEqual({postId: '123', revision: '456',});
   });
 
+  it('resolves a URL path matching multiple replacement markers in the ' +
+           'same path component', function() {
+    router.addRoute('people:multiformat', '/api/people/{personId}.{ext}',
+                    fakeView);
+    var match = router.getMatch('/api/people/123.json');
+    expect(match.route.name).toBe('people:multiformat');
+    expect(match.fields).toEqual({personId: '123', ext: 'json',});
+  });
+
   it('throws if replacement markers conflict', function() {
     expect(
       router.addRoute.bind(
-        router, 'posts:edit', '/posts/{postId}/{postId}', function() {}
+        router, 'posts:edit', '/posts/{postId}/{postId}', fakeView
       )
     ).toThrowError(/Duplicate replacement markers/);
   });
